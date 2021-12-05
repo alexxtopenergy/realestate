2d874a0caae3cbdda59af88b8883f29f124f1fbc
Popov <oleksandr.popov@sigma.software>
2021-12-06 01:06:16 +0200

    Add Font Awesome

--- wordpress/wp-content/themes/realestate-child/functions.php
+++ wordpress/wp-content/themes/realestate-child/functions.php
@@ -1,12 +1,17 @@
 <?php
 
 /**
+ * Define Script Version
+ */
+$version_date = date( 'YmdHi' );
+define( 'SCRIPT_VERSION', $version_date );
+
+
+/**
  * Include Custom CSS and Scripts
  */
 function real_estate_enqueue_styles() {
-	wp_enqueue_style( 'main-style', get_template_directory_uri() . 'inc/css/main.css', array(), _S_VERSION );
-	wp_enqueue_script( 'main-script', get_stylesheet_directory_uri() . '/assets/js/script.js', array( 'jquery' ), _S_VERSION, true );
+	wp_enqueue_style( 'main-style', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), SCRIPT_VERSION );
+	wp_enqueue_script( 'main-script', get_stylesheet_directory_uri() . '/assets/js/script.js', array( 'jquery' ), SCRIPT_VERSION, true );
 }
-
 add_action( 'wp_enqueue_scripts', 'real_estate_enqueue_styles' );
-

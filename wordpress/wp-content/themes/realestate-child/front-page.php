a8ad4295d923d678572e877bd443dfc42b1c56a5
Popov <oleksandr.popov@sigma.software>
2021-12-06 01:17:53 +0200

    Home Page Template

---
+++ wordpress/wp-content/themes/realestate-child/front-page.php
@@ -0,0 +1,13 @@
+<?php
+get_header();
+?>
+
+<div class="homepage-section">
+	<div class="container">
+		<h1 class="pt-5">Homepage...</h1>
+		<?php the_content(); ?>
+	</div>
+</div>
+
+<?php
+get_footer();

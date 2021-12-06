f9630b174910330654cf3b4605f73268c241451f
Popov <oleksandr.popov@sigma.software>
2021-12-06 02:10:21 +0200

    Function.php fix

--- wordpress/wp-content/themes/realestate-child/functions.php
+++ wordpress/wp-content/themes/realestate-child/functions.php
@@ -15,3 +15,25 @@ function real_estate_enqueue_styles() {
 	wp_enqueue_script( 'main-script', get_stylesheet_directory_uri() . '/assets/js/script.js', array( 'jquery' ), SCRIPT_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'real_estate_enqueue_styles' );
+
+/**
+ * Register Main Menu
+ */
+register_nav_menus(
+	array(
+		'main_menu' => __( 'Main Menu', 'realestate' ),
+	)
+);
+
+function register_my_widgets() {
+	register_sidebar(
+		array(
+			'name'         => 'Right Sidebar',
+			'id'           => 'right-sidebar',
+			'description'  => 'Description...',
+			'before_title' => '<h2>',
+			'after_title'  => '</h2>',
+		)
+	);
+}
+add_action( 'widgets_init', 'register_my_widgets' );
